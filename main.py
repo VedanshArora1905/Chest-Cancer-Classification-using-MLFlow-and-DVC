15a4fa5b340d5ef98563f13a662aa4f2e6e5d711
VedanshArora1905 <vedansharora1905@gmail.com>
2024-11-07 17:31:15 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ChestCancerClassifier import logger
 from ChestCancerClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChestCancerClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-#from ChestCancerClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from ChestCancerClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 #from ChestCancerClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
@@ -30,21 +30,20 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-#
-#
-#
-#
-#STAGE_NAME = "Training"
-#try: 
-#   logger.info(f"*******************")
-#   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#   model_trainer = ModelTrainingPipeline()
-#   model_trainer.main()
-#   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-#except Exception as e:
-#        logger.exception(e)
-#        raise e
-#
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
 #
 #
 #
